d4622eec6b375a33d6a944d7a47fd2f6bf1bb018
Divyagnan <divyagnan@users.noreply.github.com>
2017-12-19 17:26:27 -0500

    clean up code, implement better game detection, configure eslint

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 // packages
 import AuroraAPI from "nanoleaves";
 import axios from "axios";
-import isMonday from "date-fns/is_monday";
-import isThursday from "date-fns/is_thursday";
-import isSunday from "date-fns/is_sunday";
-import startOfToday from "date-fns/start_of_today";
 // ours
 import { token, host } from "../config";
 import colors from "./colors";
@@ -22,36 +18,21 @@ const aurora = new AuroraAPI({
  * @param {*} aurora - api to manipulate the nanoleaf
  * @param {*} network - some api to make network calls
  * @param {array} colors - array of colors for every nfl team
- * @param {function} isMonday - checks if a given date is monday
- * @param {function} isThursday - checks if a given date is thursday
- * @param {function} isSunday - checks if a given date is sunday
- * @param {function} startOfToday - generates today
  */
-const init = async (
-  aurora,
-  network,
-  colors,
-  isMonday,
-  isThursday,
-  isSunday,
-  startOfToday
-) => {
-  // get current week nfl game data
-  const { gms } = await getNFLData(network);
+const init = async (aurora, network, colors) => {
+  try {
+    // get current week nfl game data
+    const { gms } = await getNFLData(network);
 
-  // figure out what day it is and get the home and visitor team
-  // const todayGame = getTodayGame(isMonday, isThursday, isSunday, startOfToday);
-  const { h, v } = await getTodayGame(
-    gms,
-    isMonday,
-    isThursday,
-    isSunday,
-    startOfToday
-  );
-
-  // set the colors of the teams that are playing today
-  setTeamLayout(aurora, colors, h, v);
+    // figure out what day it is and get the home and visitor team
+    // const todayGame = getTodayGame(isMonday, isThursday, isSunday, startOfToday);
+    const { h, v } = await getTodayGame(gms);
+    // set the colors of the teams that are playing today
+    setTeamLayout(aurora, colors, h, v);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 // wire everything up and make it work!
-init(aurora, axios, colors, isMonday, isThursday, isSunday, startOfToday);
+init(aurora, axios, colors);
--- src/nfl.js
+++ src/nfl.js
@@ -1,3 +1,8 @@
+import isMonday from "date-fns/is_monday";
+import isThursday from "date-fns/is_thursday";
+import isSunday from "date-fns/is_sunday";
+import startOfToday from "date-fns/start_of_today";
+
 /**
  * Get the list of games/scores from the nfl
  * @param {*} network - some api to make network calls
@@ -7,37 +12,40 @@ export const getNFLData = async network => {
   const response = await network.get(
     "http://www.nfl.com/liveupdate/scorestrip/ss.json"
   );
-  const data = await response.data;
-  return data;
+
+  return response.data;
 };
 
 /**
  * Get today's game from a list of all games
+ * (or in the event that today is not game day, get the next closest game)
  * @param {array} games - the array of games this week
- * @param {function} isMonday - checks if a given date is monday
- * @param {function} isThursday - checks if a given date is thursday
- * @param {function} isSunday - checks if a given date is sunday
- * @param {function} startOfToday - generates today
  * @returns {object} data for todays game (who is playing, score, etc)
  */
-export const getTodayGame = (
-  games,
-  isMonday,
-  isThursday,
-  isSunday,
-  startOfToday
-) => {
+export const getTodayGame = games => {
   const today = startOfToday();
-  let game;
   if (isMonday(today)) {
     // today is monday! mnf :)
-    game = games.find(game => today.toString().includes("Mon"));
+    return games.find(game => game.d.includes("Mon"));
   } else if (isThursday(today)) {
     // today is thursday! tnf :(
-    game = games.find(game => today.toString().includes("Thu"));
-  } else {
+    return games.find(game => game.d.includes("Thu"));
+  } else if (isSunday(today)) {
     // today is sunday! game day :)
-    game = games.find(game => today.toString().includes("Sun"));
+    return games.find(game => game.d.includes("Sun"));
+  } else {
+    // no games today :( so get the next unplayed game
+    // find the first game without a home and visitor score (since that means it hasn't been played yet)
+    const potentialGame = games.find(
+      game => typeof game.hs === "undefined" && typeof game.vs === "undefined"
+    );
+
+    // if the potential game is not undefined that means there is an unplayed game this week
+    if (typeof potentialGame !== "undefined") {
+      return potentialGame;
+    } else {
+      // pick the last game in the list since all games have been played
+      return games.slice(-1)[0];
+    }
   }
-  return game;
 };
